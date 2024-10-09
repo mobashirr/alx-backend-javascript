ecb51303eba56e67a34f3a2b6a70d92b48254ae1
sozoia <zfzg2232@gmail.com>
2024-10-09 21:34:19 +0300

    task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
 
-export default function concatArrays(...args) {
-  return args.reduce((holder, item) => {
-    if (typeof item === 'string') {
-      return [...holder, ...item];
-    } else if (Array.isArray(item)) {
-      return [...holder, ...item.reduce((innerholder, el) => 
-        typeof el === 'string' ? [...innerholder, ...el] : [...innerholder, el]
-      , [])];
-    } else {
-      return [...holder, item];
-    }
-  }, []); // The initial value of `holder` is an empty array
-
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
