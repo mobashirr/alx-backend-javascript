9120c97edce5a3de56bb06b671c1f5053dccc47e
sozoia <zfzg2232@gmail.com>
2024-10-11 21:09:52 +0300

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [idx, value] of array.entires()) {
+    array[idx] = appendString + value;
+  }
+  return array;
+}
