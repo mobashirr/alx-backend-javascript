ca6797a184f559ef66017a617ea670913d5777ab
sozoia <zfzg2232@gmail.com>
2024-10-11 21:02:06 +0300

    task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
+
+  
