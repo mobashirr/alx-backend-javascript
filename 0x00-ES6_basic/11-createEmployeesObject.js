aaee3d618e8d69cdc7390397962c3802d390e961
sozoia <zfzg2232@gmail.com>
2024-10-11 21:13:47 +0300

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+export default function createEmployeesObject(departmentName, employees) {
+
+  EmployeesObect = {
+    '${departmentName}' : [...employees]
+  };
+  return EmployeesObect;
+}
