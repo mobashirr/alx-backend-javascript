4d8953d97884e97a41c020dd5323691cd98db1fc
sozoia <zfzg2232@gmail.com>
2024-10-11 23:20:28 +0300

    task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+
+export default function createReportObject(employeesList) {
+	EmployeesObject = {
+		'allEmployees':{
+			...employeesList
+		}, getNumberOfDepartments (employeesList) {
+			return Object.keys(employeesList).length;
+		}
+	}
+	return EmployeesObject;
+}
