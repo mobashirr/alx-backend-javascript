91e3b33db102a47938b24aa6d68f5b230e4f0f54
sozoia <zfzg2232@gmail.com>
2024-10-19 11:36:22 +0300

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+
+        if (True) {
+            resolve('done')
+        }
+        else {
+            reject('error')
+        }
+    })
+}
