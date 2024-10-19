afe4a0be7019454a52b4936c1c99799a134b735a
sozoia <zfzg2232@gmail.com>
2024-10-19 12:05:42 +0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      let result = { status: 200, body: 'success' };
+      return result;
+    })
+    .catch(() => {
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
