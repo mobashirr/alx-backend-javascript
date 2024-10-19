d896fcf9d20c6a4ff4dcdfe76df09c2cfa5cf0ed
sozoia <zfzg2232@gmail.com>
2024-10-19 17:55:49 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+import { uploadPhoto, createUser } from './utils'; // Use ES6 import
+
+function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => 
+    { console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+
+export default handleProfileSignup
