ca49ae6b5d8c4342f72436caf69671a91855fda5
sozoia <zfzg2232@gmail.com>
2024-10-19 12:18:06 +0300

    add export line

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -14,3 +14,5 @@ function getFullResponseFromAPI(success){
     }
   });
 };
+
+export default getFullResponseFromAPI;
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,8 @@
-#!/usr/bin/node
-
 function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      let result = { status: 200, body: 'success' };
-      return result;
-    })
-    .catch(() => {
-      return new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
+
+export default handleResponseFromAPI;
