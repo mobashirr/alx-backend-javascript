3cb04557f6bda3a2b1b5a66bc59f21884a0dcd19
sozoia <zfzg2232@gmail.com>
2024-10-19 19:38:11 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default async function handleProfileSignup(firstname, lastname, filename) {
+  const finalProfileState = [];
+  
+  const results = await Promise.allSettled([
+    signUpUser(firstname, lastname),
+    uploadPhoto(filename),
+  ]);
+
+  results.forEach((result) => {
+    if (result.status === 'fulfilled') {
+      finalProfileState.push({
+        status: result.status,
+        value: result.value,
+      });
+    } else {
+      // For rejected promises, return the reason as value
+      finalProfileState.push({
+        status: result.status,
+        value: result.reason,
+      });
+    }
+  });
+
+  return finalProfileState;
+}
