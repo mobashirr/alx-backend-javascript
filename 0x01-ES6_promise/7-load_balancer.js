7a33ac5383dd98ddfb91d7070684c48d404f20d7
sozoia <zfzg2232@gmail.com>
2024-10-22 09:47:57 +0300

    task 7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+function loadBalancer(chinaDownload, USDownload) {
+    return Promise
+      .race([chinaDownload, USDownload])
+      .then((res) => res);
+  }
+  
+  export default loadBalancer;
