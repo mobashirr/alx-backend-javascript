ef40aba76f89911d8195b05cd797888ec52a52ea
sozoia <zfzg2232@gmail.com>
2024-10-22 10:00:00 +0300

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
+
+export default guardrail;
