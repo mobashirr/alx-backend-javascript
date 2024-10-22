1411fe141d748d42ec5a7752e4b479ba0104ba3c
sozoia <zfzg2232@gmail.com>
2024-10-22 11:08:43 +0300

    setupfiles

---
+++ 0x02-ES6_classes/setup.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Function to create package.json
+create_package_json() {
+  cat <<EOL > package.json
+{
+  "scripts": {
+    "lint": "./node_modules/.bin/eslint",
+    "check-lint": "lint [0-9]*.js",
+    "dev": "npx babel-node",
+    "test": "jest",
+    "full-test": "./node_modules/.bin/eslint [0-9]*.js && jest"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.6.0",
+    "@babel/preset-env": "^7.6.0",
+    "@babel/node": "^7.8.0",
+    "eslint": "^6.4.0",
+    "eslint-config-airbnb-base": "^14.0.0",
+    "eslint-plugin-import": "^2.18.2",
+    "eslint-plugin-jest": "^22.17.0",
+    "jest": "^24.9.0"
+  }
+}
+EOL
+  echo "package.json created"
+}
+
+# Function to create babel.config.js
+create_babel_config() {
+  cat <<EOL > babel.config.js
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
+EOL
+  echo "babel.config.js created"
+}
+
+# Function to create .eslintrc.js
+create_eslint_config() {
+  cat <<EOL > .eslintrc.js
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
+EOL
+  echo ".eslintrc.js created"
+}
+
+# Main function to call file creation functions
+main() {
+  create_package_json
+  create_babel_config
+  create_eslint_config
+  echo "All configuration files created successfully."
+}
+
+# Call main function
+main
