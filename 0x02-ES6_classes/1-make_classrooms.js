61680af66ac1339012ae18bc1a4261cbc64f5203
sozoia <zfzg2232@gmail.com>
2024-10-22 11:21:37 +0300

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
+
+export default initializeRooms;
