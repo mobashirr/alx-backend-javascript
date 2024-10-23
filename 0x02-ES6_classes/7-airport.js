df83bced7b9be4e44a6a29d56db624e2a6a866e4
sozoia <zfzg2232@gmail.com>
2024-10-23 17:38:18 +0300

    task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+#!/usr/bin/nodes
+
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    // this will change the default string description to [object code]
+    // the symbol.tostringtag is known symbol to do this
+    return this._code;
+  }
+}
