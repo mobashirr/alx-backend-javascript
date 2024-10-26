c58957d7dfe2bd987a570fbdfd5cc1fbbb2f18d1
sozoia <zfzg2232@gmail.com>
2024-10-26 11:03:53 +0300

    export statment

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@
 
 import Car from "./10-car";
 
-class EVCar extends Car {
+export default class EVCar extends Car {
     constructor(brand, motor, color, range) {
       super(brand, motor, color);
       this.range = range;
