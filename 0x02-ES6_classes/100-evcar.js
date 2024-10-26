ca65143220bf2b932f0b9005add99a10f057a63b
sozoia <zfzg2232@gmail.com>
2024-10-26 11:02:12 +0300

    task 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+
+import Car from "./10-car";
+
+class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+      super(brand, motor, color);
+      this.range = range;
+    }
+  
+    get range() {
+      return this._range;
+    }
+  
+    set range(value) {
+      this._range = value;
+    }
+
+    cloneCar() {
+      const Species = super.constructor[Symbol.species];
+  
+      return new Species(this.brand, this.motor, this.color);
+    }
+  }
