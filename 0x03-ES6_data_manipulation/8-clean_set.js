b09c5f2853b260752c4b5b3f768eeebf9c7cd24b
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-10-28 21:08:10 +0000

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+export default function cleanSet(set, startString) {
+  const result = [];
+
+  set.array.forEach((element) => {
+    if (element.startsWith(startString)) {
+      result.concat([element]);
+    }
+  });
+  return result;
+}
