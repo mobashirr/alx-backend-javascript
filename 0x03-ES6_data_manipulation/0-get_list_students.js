2f2ebbaeb38593a42162e126f93eb1ef3ef6e5a9
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-10-28 17:45:18 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,4 +4,7 @@ function getListStudents() {
   obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   obj2 = { id: 2, firstName: 'James', location: 'Columbia' },
   obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' }
+
+  result = [].concat(obj1,obj2,obj3)
+  return result
 }
