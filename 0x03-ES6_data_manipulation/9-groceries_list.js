b6fc6549e20c25fda4dcdee11ab835b94572a331
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-10-28 21:20:32 +0000

    task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+export default function cleanSet(set, startString) {
+  const parts = [];
+  // validate inputs
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  // iterate over all value of given set
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      // if the current value start with our string pattern save the current val
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        // check that the current val is not empty after slice
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  // join all matched strings into parts array
+  return parts.join('-');
+}
