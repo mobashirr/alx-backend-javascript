750afd844b1a1ba41e78c17757b1c1c20547e0f2
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-10-28 17:57:48 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-export default function getListStudents() {
-  obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  obj2 = { id: 2, firstName: 'James', location: 'Columbia' },
-  obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' }
-
-  result = [obj1,obj2,obj3]
-  return result
+export default function getListStudents () {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
