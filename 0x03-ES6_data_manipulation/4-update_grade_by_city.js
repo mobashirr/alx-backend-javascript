c1d19bb44a783851a3005de10b7900b71daaa83b
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-10-28 20:47:25 +0000

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students.filter((student) =>
+    student.location === city).map((student) =>
+    ({
+      id: student.id,
+      firstname: student.firstName,
+      location: student.location,
+      grade: (newGrades.filter((grade) => grade.studentId === student.id)[0] || defGrade).grade,
+    }));
+  }
+  return [];
+}
