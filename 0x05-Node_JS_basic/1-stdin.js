242aefde9eaeaaf46fca52e17fbab74bc7d981ae
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-11-27 20:54:34 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const readline = require('readline')
+
+
+// because node is one-threaded, normal I/O opreations mustn't block that thread
+// so its handeled using async opreaions that provided by readline module
+// we have to create an interface to read from the console input
+bridge = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+// using the interface to access the inupt of the console 
+bridge.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log('your name is: ' + name)
+    bridge.close()
+})
