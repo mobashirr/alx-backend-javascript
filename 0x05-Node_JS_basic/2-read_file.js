48ffdb586a76e4885719b7a353d60e5af19ae238
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-11-29 23:22:16 +0000

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,21 +3,31 @@
 const fs = require('fs')
 
 function countStudents(path) {
-
     try {
         const data = fs.readFileSync(path,'utf-8').split('\n')
         const number_of_std = data.length - 1
-        let maped_data = {}
-        console.log(data.length)
+        let maped_data = new Map()
 
-        for (let i = 1; i < data.length; i++) {
-            const std = data[i].split(',')
-            if (std[4] in maped_data) { maped_data[std[4]].append(std)}
-            else {maped_data[std[4]] = [std]}
-            console.log(data[i])
+        // iterate over students
+        for (std_data of data.slice(1)) {
+            const fields = std_data.split(',')
+
+            if (maped_data.has(fields[3])) {
+                // add the student to the field if its exist
+                maped_data.get(fields[3]).push(fields[0])
+                
+            } else {
+                // or if its not in the map set the field with new value
+                maped_data.set(fields[3], [fields[0]])
+            }
         }
-        console.log(maped_data)
 
+        console.log(`Number of students: ${number_of_std}`)
+
+        // print students by fields
+        for ([key,val] of maped_data) {
+            console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`)
+        }
     }
     catch (err) {
         console.log(err)
