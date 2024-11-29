242b081d6f7873ceec2e97359119d56fb80f1039
MOHAMED BASHIR <138500056+mobashirr@users.noreply.github.com>
2024-11-29 23:31:17 +0000

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,49 @@
 #!/usr/bin/node
+
+const http = require('http');
+const countStudents = require('./3-read_file_async'); // Import the asynchronous function
+const url = require('url');
+
+// Create the server
+const app = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url, true);
+
+  if (pathname === '/') {
+    // Handle root URL
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (pathname === '/students') {
+    // Handle /students URL
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write('This is the list of our students\n');
+
+    // Get the database file path from command line arguments
+    const databasePath = process.argv[2];
+
+    // Use countStudents to process the database file
+    countStudents(databasePath)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    // Handle undefined routes
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+// Make the server listen on port 1245
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// Export the app
+module.exports = app;
